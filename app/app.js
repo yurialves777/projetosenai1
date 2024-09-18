d5df5777f621a7c3ef93a85c647430afa3079441
yurialves777 <yurialvesschwambach@gmail.com>
2024-09-17 22:21:00 -0300

    requisiçoes GET e POST

--- app/app.js
+++ app/app.js
@@ -1,12 +1,30 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const port = 8080
+const path = require('path');
 
-app.get('/sobre-20', (req,res) => {
-    res.send("<h1>titulo</h1>")
+// Middleware para habilitar o parsing de JSON no body
+app.use(express.json());
+
+// Serve the 'index.html' file from the root
+app.get('/api-tester', (req, res) => {
+  res.sendFile(path.join(__dirname, 'publico/index.html'));
+});
+
+app.get('/', (req, res) => {
+  res.send('Funcionou essa p****!')
+})
+
+//Cole sua rota POST aqui
+app.post('/dados', (req, res) => {
+    const dados = req.body; // Acessa os dados enviados no corpo da requisição
+    res.send(`Dados recebidos: ${JSON.stringify(dados)}`);
+});
+app.post('/', (req, res) => {
+    console.log('Dados recebidos no body:', req.body);
+    res.json({ message: 'Dados recebidos com sucesso!', body: req.body });
 })
 
 app.listen(port, () => {
-    console.log("ESta funcionando")
-
+  console.log(`Example app listening on port ${port}`)
 })
